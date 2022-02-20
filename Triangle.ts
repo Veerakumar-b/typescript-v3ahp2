1fd795069a3074e89466d048e153dd284eb70ec3
Veerakumar-b <kumar.ch088@gmail.com>
2022-02-20 14:46:46 +0530

    2 files modified

--- Triangle.ts
+++ Triangle.ts
@@ -18,6 +18,17 @@ export class Line {
   }
 }
 
+export class Triangle{
+  line1 : Line;
+  line2 : Line;
+  line3 : Line;
+
+  constructor(line1 : Line,line2 : Line, line3 : Line){
+    this.line1 = line1;
+    this.line2 = line2;
+    this.line3 = line3;
+  }
+}
 // class Triangle{
 //   dot1 : Dot (start : Dot,end : Dot);
 //   dot2 : Dot (start : Dot,end : Dot);
--- index.ts
+++ index.ts
@@ -1,17 +1,20 @@
 // Import stylesheets
 import './style.css';
 
-import { Dot } from './Triangle';
+import { Dot,Line,Triangle } from './Triangle';
 
 let point1 = new Dot(3.5, 2);
+let point2 = new Dot(2.5,4);
+let point3 = new Dot(4.5,4);
 
-console.dir('point1');
+let line1 = new Line(point1, point2);
+let line2 = new Line(point2, point3);
+let line3 = new Line(point3, point1);
 
-import { Line } from './Triangle';
+let triangleobj = new Triangle(line1, line2, line3);
+console.log(triangleobj);
 
-let line1 = new Line(point1, point1);
 
-console.dir(line1);
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
